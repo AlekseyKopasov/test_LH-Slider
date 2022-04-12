116336f52b50a5a0d74d07693d8e3c262810b9a9
Aleksey Kopasov <alesey.k1@gmail.com>
2022-04-13 00:51:52 +0300

    WIP: фиксирует позицию любого слайда по клику

--- js/slider.js
+++ js/slider.js
@@ -25,27 +25,49 @@ $(document).ready(function() {
         }
       }
     ],
-
   });
 
   $('.slide').on('click', function(evt) {
-    $('.locked').removeClass('locked');
-
-    if ($(evt.currentTarget).hasClass('locked')) {
-      console.log(42);
-      $(evt.currentTarget).removeClass('locked');
-    } else {
-      $(evt.currentTarget).addClass('locked');
+    if ($('.locked')[0] !== $(evt.currentTarget)[0]) {
+      $('.locked').toggleClass('locked');
     }
 
-
-    // let lockedSlide = $(this).toggleClass('locked');
-    // let lockedSlideIdx = $(evt.currentTarget).attr('data-slick-index');
+    $(evt.currentTarget).hasClass('locked') ?
+      $(evt.currentTarget).removeClass('locked') :
+      $(evt.currentTarget).addClass('locked');
   });
 
-  $('.slick-arrow').click(function () {
-    let isLocked = !!$('.locked').length;
-    console.log(isLocked)
-  })
+  $('.slick-arrow').on('click', (function (evt) {
+    let isLocked = $('.locked').length;
+    if (!isLocked) {
+      return;
+    }
 
+    let lockedSlide = $('.locked')[0];
+    let lockedSlideIndex = $('.locked').attr("data-slick-index");
+    let nextSlide = $(`[data-slick-index=${+lockedSlideIndex + 1}]`)[0];
+    let prevSlide = $(`[data-slick-index=${+lockedSlideIndex - 1}]`)[0];
+    const nextButton = $(evt.currentTarget).hasClass('slick-next');
+
+    let clonedLocked = lockedSlide.cloneNode(true); // 1
+    let clonedNext = nextSlide.cloneNode(true); // 2
+    let clonedPrev = prevSlide.cloneNode(true);  // 3
+
+    if (nextButton) {
+      let nextIndex = $(nextSlide).attr("data-slick-index");
+
+      nextSlide.parentNode.replaceChild(clonedLocked, nextSlide);
+      lockedSlide.parentNode.replaceChild(clonedNext, lockedSlide);
+
+      nextSlide = $(`[data-slick-index=${(+nextIndex + 1)}]`)[0];
+
+    } else {
+      let prevIndex = $(prevSlide).attr("data-slick-index");
+
+      prevSlide.parentNode.replaceChild(clonedLocked, prevSlide);
+      lockedSlide.parentNode.replaceChild(clonedPrev, lockedSlide);
+
+      prevSlide = $(`[data-slick-index=${(+prevSlide + 1)}]`)[0];
+    }
+  }));
 });
